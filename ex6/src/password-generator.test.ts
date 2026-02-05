618f34181bf767053841b627f22e3dba624db1d3
Dilan Villada <dvilladamejia@gmail.com>
2026-02-05 15:22:43 -0500

    ex6_server

---
+++ ex6/src/password-generator.test.ts
@@ -0,0 +1,20 @@
+import  generatePassword  from "./password-generator";
+
+describe("generatePassword", () => {
+    test("creates a password with 6 characters by default" , () =>{
+        const result: string = generatePassword();
+        expect(result.length).toBe(6);
+    });
+
+    test("password is a string", () => {
+        const password = generatePassword();
+        expect(typeof password).toBe("string");
+    });
+
+    test("throws an error if length is greater that 20", () =>{
+        expect(() => {
+            generatePassword(21);
+        }).toThrow();
+    });
+
+});
